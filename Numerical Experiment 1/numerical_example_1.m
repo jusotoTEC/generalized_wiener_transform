5bc28961450d7545574ad739be58b77cb1e44908
jusotoTEC <102713314+jusotoTEC@users.noreply.github.com>
2023-03-10 10:35:55 -0600

    Update numerical_example_1.m

--- Numerical Experiment 1/numerical_example_1.m
+++ Numerical Experiment 1/numerical_example_1.m
@@ -3,7 +3,7 @@
 % Reference: 
 %   Paper   = A multi-filtering transform of random vectors: theoretical and
 %             computational aspect. (Submitted paper - 2023)
-%   Authors = Soto-Quiros
+%   Authors = Soto-Quiros, P.
 
 clc; clear
 warning ('off','all');
