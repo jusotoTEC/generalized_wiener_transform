ce099e150979f76b0834bdca7141ad5ed3ffbdf2
jusotoTEC <102713314+jusotoTEC@users.noreply.github.com>
2023-03-10 10:37:30 -0600

    Add files via upload

---
+++ Numerical Experiment 2/mftNaive.m
@@ -0,0 +1,16 @@
+function F=mftNaive(Exy,Eyy)
+    [m,n,p]=size(Eyy);
+    A=zeros(m,n*p); B=zeros(n,n*p);
+    for i=1:p
+        EyiyiSqrt=sqrtmSVD(Eyy(:,:,i));
+        EyiyiSqrtPinv=pinv(EyiyiSqrt);
+        A(:,(i-1)*n+1:i*n)=Exy(:,:,i)*EyiyiSqrtPinv;
+        B(:,(i-1)*n+1:i*n)=EyiyiSqrt;
+    end
+    F=A*pinv(B);
+end
+
+function X=sqrtmSVD(A)
+    [U,S,V]=svd(A);
+    X=U*sqrt(S)*V';
+end
---
+++ Numerical Experiment 2/numerical_example_2.m
@@ -0,0 +1,78 @@
+% Numerical Experiment 2
+
+% Reference: 
+%   Paper   = A multi-filtering transform of random vectors: theoretical and
+%             computational aspect. (Submitted paper - 2023)
+%   Authors = Soto-Quiros, Pablo
+
+clc; clear; close all
+
+%%%% Step 1: Specify dimensions %%%%
+
+p=10; dim=500:500:2000;
+
+timesM1=zeros(length(dim),1);
+timesM2=zeros(length(dim),1);
+speedup=zeros(length(dim),1);
+percent_difference=zeros(length(dim),1);
+
+k=0;
+
+for m=dim
+    k=k+1;
+    
+    %%%% Step 2: Compute the covariance matrices %%%%
+    
+    Exx=zeros(m,m,p);
+    Exy=zeros(m,m,p);
+    Eyy=zeros(m,m,p);
+    for i=1:p
+        rho=rand(1);
+        Exixi=rho*ones(m,m)+diag(ones(m,1)-rho*ones(m,1));
+        Exx(:,:,i)=Exixi;
+        A=rand(m);
+        Exy(:,:,i)=Exixi*A';
+        sigma=10*rand(1);
+        Enini=sigma^2*eye(m);
+        Eyy(:,:,i)=A*Exixi*A'+Enini;
+    end
+    %%%% Step 3: Comuputing the multi-filtering transform using fast implementation and original formula %%%%
+    
+    tic; F1=mft(Exy,Eyy); timesM1(k)=toc;    
+    tic; F=mftNaive(Exy,Eyy); timesM2(k)=toc;    
+    speedup(k)=timesM2(k)/timesM1(k);
+    percent_difference(k)=100*(timesM2(k)-timesM1(k))/timesM2(k);    
+end
+
+%%%% Step 4: Display the results obtained  %%%%
+
+hold on
+plot(dim,timesM1,"b")
+plot(dim,timesM2,"r")
+set(gca,'FontSize',14)
+xlabel('Dimension (m)','FontSize',14)
+ylabel('Execution Time (s)','FontSize',14)
+grid on
+legend('Formula (18)', 'Theorem 1')
+
+figure
+plot(dim,timesM1,"b")
+set(gca,'FontSize',14)
+xlabel('Dimension (m)','FontSize',14)
+ylabel('Execution Time (s)','FontSize',14)
+grid on
+legend('Formula (18)')
+
+figure
+plot(dim,speedup)
+set(gca,'FontSize',14)
+xlabel('Dimension (m)','FontSize',14)
+ylabel('Speedup','FontSize',14)
+grid on
+
+figure
+plot(dim,percent_difference)
+set(gca,'FontSize',14)
+xlabel('Dimension (m)','FontSize',14)
+ylabel('Percent Diference','FontSize',14)
+grid on
